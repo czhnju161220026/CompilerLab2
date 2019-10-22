086ba0032397df22c2d98ea6f4cda278122b2fe0
cuizihan <cui@localhost.localdomain>
2019-10-22 15:19:31 +0800

    code format

--- Code/grammarTree.c
+++ Code/grammarTree.c
@@ -1,42 +1,44 @@
 #include "grammarTree.h"
 #include <stdbool.h>
 
-const char *const TYPES_NAME_TABLE[] = 
-{
-    /*0 Blank*/
-	"BLANK",
-	/*1 Tokens*/
-	"SEMI","COMMA","ASSIGNOP","RELOP",
-	"PLUS","MINUS","STAR","DIV",
-	"AND","OR","DOT","NOT","TYPE",
-	"LP","RP","LB","RB","LC","RC",
-	"STRUCT","RETURN","IF","ELSE","WHILE",
-	"ID","INT","FLOAT",
-	/*2 High-level Definitions*/
-	"Program","ExtDefList","ExtDef","ExtDecList",
-	/*3 Specifiers*/
-	"Specifier","StructSpecifier","OptTag","Tag",
-	/*4 Declarators*/
-	"VarDec","FunDec","VarList","ParamDec",
-	/*5 Statements*/
-	"CompSt","StmtList","Stmt",
-	/*6 Local Definitions*/
-	"DefList","Def","DecList","Dec",
-	/*7 Expressions*/
-	"Exp","Args"
-};
+const char *const TYPES_NAME_TABLE[] =
+    {
+        /*0 Blank*/
+        "BLANK",
+        /*1 Tokens*/
+        "SEMI", "COMMA", "ASSIGNOP", "RELOP",
+        "PLUS", "MINUS", "STAR", "DIV",
+        "AND", "OR", "DOT", "NOT", "TYPE",
+        "LP", "RP", "LB", "RB", "LC", "RC",
+        "STRUCT", "RETURN", "IF", "ELSE", "WHILE",
+        "ID", "INT", "FLOAT",
+        /*2 High-level Definitions*/
+        "Program", "ExtDefList", "ExtDef", "ExtDecList",
+        /*3 Specifiers*/
+        "Specifier", "StructSpecifier", "OptTag", "Tag",
+        /*4 Declarators*/
+        "VarDec", "FunDec", "VarList", "ParamDec",
+        /*5 Statements*/
+        "CompSt", "StmtList", "Stmt",
+        /*6 Local Definitions*/
+        "DefList", "Def", "DecList", "Dec",
+        /*7 Expressions*/
+        "Exp", "Args"};
 
-const char* typeToString(Types type) {
+const char *typeToString(Types type)
+{
     return TYPES_NAME_TABLE[type];
 }
 
-Morpheme* createMorpheme(Types type) {
-    Morpheme* morpheme = (Morpheme*) malloc(sizeof(Morpheme));
-    if (morpheme == NULL) {
+Morpheme *createMorpheme(Types type)
+{
+    Morpheme *morpheme = (Morpheme *)malloc(sizeof(Morpheme));
+    if (morpheme == NULL)
+    {
         printf("Memory alloc error\n");
         exit(0);
     }
-    
+
     morpheme->type = type;
     morpheme->father = NULL;
     morpheme->child = NULL;
@@ -49,14 +51,17 @@ Morpheme* createMorpheme(Types type) {
     return morpheme;
 }
 
-void destructMorpheme(Morpheme* morpheme) {
-    if (morpheme == NULL) {
+void destructMorpheme(Morpheme *morpheme)
+{
+    if (morpheme == NULL)
+    {
         return;
     }
 
     int i = 0;
-    Morpheme* m = morpheme->child;
-    while (m != NULL) {
+    Morpheme *m = morpheme->child;
+    while (m != NULL)
+    {
         destructMorpheme(m);
         m = m->siblings;
     }
@@ -64,22 +69,28 @@ void destructMorpheme(Morpheme* morpheme) {
         destructMorpheme(morpheme->children[i]);
         i++;
     }*/
-    if (morpheme->type == _RELOP || morpheme->type == _ID || morpheme->type == _TYPE) {
+    if (morpheme->type == _RELOP || morpheme->type == _ID || morpheme->type == _TYPE)
+    {
         free(morpheme->idName);
     }
     //printf("%s\n", typeToString(morpheme->type));
     free(morpheme);
 }
 
-void addChild(Morpheme* father, Morpheme* child) {
+void addChild(Morpheme *father, Morpheme *child)
+{
     /*if (i >= MAX_CHILDREN_NUMBER) {
         printf("max children number overflow\n");
     }*/
-    if (father->child == NULL) {
+    if (father->child == NULL)
+    {
         father->child = child;
-    } else {
-        Morpheme* c = father->child;
-        while (c->siblings != NULL) {
+    }
+    else
+    {
+        Morpheme *c = father->child;
+        while (c->siblings != NULL)
+        {
             c = c->siblings;
         }
         c->siblings = child;
@@ -87,57 +98,76 @@ void addChild(Morpheme* father, Morpheme* child) {
 
     //father->children[i] = child;
     child->father = father;
-    Morpheme* f= father;
-    if (child->lineNumber != -1) {
-        while (f != NULL && f->lineNumber == -1) {
+    Morpheme *f = father;
+    if (child->lineNumber != -1)
+    {
+        while (f != NULL && f->lineNumber == -1)
+        {
             f->lineNumber = child->lineNumber;
             f = f->father;
         }
     }
 }
 
-
-void nodeGrowth(Morpheme* father, int n, ...) {
+void nodeGrowth(Morpheme *father, int n, ...)
+{
     va_list argp;
-    va_start(argp,n);
-    for (int i = 0; i < n; i++) {
-        addChild(father, va_arg(argp, Morpheme*));
+    va_start(argp, n);
+    for (int i = 0; i < n; i++)
+    {
+        addChild(father, va_arg(argp, Morpheme *));
     }
 }
 
-
-
-void printGrammarTree(Morpheme* root, int depth) {
+void printGrammarTree(Morpheme *root, int depth)
+{
     //printf("in\n");
-    if (root == NULL) {
+    if (root == NULL)
+    {
         return;
     }
-    if (root->type == _BLANK) {
+    if (root->type == _BLANK)
+    {
         return;
     }
-    if (root->lineNumber != -1) {
-        for (int k = 0; k < depth; k++) {
+    if (root->lineNumber != -1)
+    {
+        for (int k = 0; k < depth; k++)
+        {
             printf("  ");
         }
-        if (root->type >= 28) {
+        if (root->type >= 28)
+        {
             printf("%s (%d)\n", typeToString(root->type), root->lineNumber);
-        } else if (root->type <= 27 && root->type >= 1) {
-            if (root->type == _ID) {
+        }
+        else if (root->type <= 27 && root->type >= 1)
+        {
+            if (root->type == _ID)
+            {
                 printf("%s: %s\n", typeToString(root->type), root->idName);
-            } else if (root->type == _INT) {
+            }
+            else if (root->type == _INT)
+            {
                 printf("%s: %d\n", typeToString(root->type), root->intValue);
-            } else if (root->type == _FLOAT) {
+            }
+            else if (root->type == _FLOAT)
+            {
                 printf("%s: %f\n", typeToString(root->type), root->floatValue);
-            } else if (root->type == _TYPE) {
+            }
+            else if (root->type == _TYPE)
+            {
                 printf("%s: %s\n", typeToString(root->type), root->idName);
-            } else {
+            }
+            else
+            {
                 printf("%s\n", typeToString(root->type));
             }
         }
-    
+
         int i = 0;
-        Morpheme* c = root->child;
-        while (c != NULL) {
+        Morpheme *c = root->child;
+        while (c != NULL)
+        {
             printGrammarTree(c, depth + 1);
             c = c->siblings;
         }
@@ -146,6 +176,6 @@ void printGrammarTree(Morpheme* root, int depth) {
             i++;
         }*/
     }
-    
+
     //printf("out %d\n", i);
 }
--- Code/grammarTree.h
+++ Code/grammarTree.h
@@ -14,46 +14,80 @@ typedef enum Types
 	/*0 Blank*/
 	_BLANK,
 	/*1 Tokens*/
-	_SEMI,_COMMA,_ASSIGNOP,_RELOP,
-	_PLUS,_MINUS,_STAR,_DIV,
-	_AND,_OR,_DOT,_NOT,_TYPE,
-	_LP,_RP,_LB,_RB,_LC,_RC,
-	_STRUCT,_RETURN,_IF,_ELSE,_WHILE,
-	_ID,_INT,_FLOAT,
+	_SEMI,
+	_COMMA,
+	_ASSIGNOP,
+	_RELOP,
+	_PLUS,
+	_MINUS,
+	_STAR,
+	_DIV,
+	_AND,
+	_OR,
+	_DOT,
+	_NOT,
+	_TYPE,
+	_LP,
+	_RP,
+	_LB,
+	_RB,
+	_LC,
+	_RC,
+	_STRUCT,
+	_RETURN,
+	_IF,
+	_ELSE,
+	_WHILE,
+	_ID,
+	_INT,
+	_FLOAT,
 	/*2 High-level Definitions*/
-	_Program,_ExtDefList,_ExtDef,_ExtDecList,
+	_Program,
+	_ExtDefList,
+	_ExtDef,
+	_ExtDecList,
 	/*3 Specifiers*/
-	_Specifier,_StructSpecifier,_OptTag,_Tag,
+	_Specifier,
+	_StructSpecifier,
+	_OptTag,
+	_Tag,
 	/*4 Declarators*/
-	_VarDec,_FunDec,_VarList,_ParamDec,
+	_VarDec,
+	_FunDec,
+	_VarList,
+	_ParamDec,
 	/*5 Statements*/
-	_CompSt,_StmtList,_Stmt,
+	_CompSt,
+	_StmtList,
+	_Stmt,
 	/*6 Local Definitions*/
-	_DefList,_Def,_DecList,_Dec,
+	_DefList,
+	_Def,
+	_DecList,
+	_Dec,
 	/*7 Expressions*/
-	_Exp,_Args
-}Types;
+	_Exp,
+	_Args
+} Types;
 
+typedef struct Morpheme
+{
+	Types type;
+	union {
+		char *idName;
+		int intValue;
+		float floatValue;
+	};
+	int lineNumber;
+	struct Morpheme *father;
+	//struct Morpheme* children[MAX_CHILDREN_NUMBER];
+	struct Morpheme *child;
+	struct Morpheme *siblings;
+} Morpheme;
 
-
-typedef struct Morpheme {
-    Types type;
-    union 
-    {
-        char* idName;
-        int intValue;
-        float floatValue;
-    };
-    int lineNumber;
-    struct Morpheme* father;
-    //struct Morpheme* children[MAX_CHILDREN_NUMBER];
-	struct Morpheme* child;
-    struct Morpheme* siblings;
-}Morpheme;
-
-Morpheme* createMorpheme(Types type);
-void nodeGrowth(Morpheme* father, int n, ...);
-void printGrammarTree(Morpheme* root, int depth);
-void destructMorpheme(Morpheme* morpheme);
-const char* typeToString(Types type);
+Morpheme *createMorpheme(Types type);
+void nodeGrowth(Morpheme *father, int n, ...);
+void printGrammarTree(Morpheme *root, int depth);
+void destructMorpheme(Morpheme *morpheme);
+const char *typeToString(Types type);
 #endif
